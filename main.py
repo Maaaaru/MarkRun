65712866a5af6a9494ca226d6f1c1972fbfe5cb7
Maaaaru <kou0407hei@gmail.com>
2022-10-08 23:48:46 +0900

    :adhesive_bandage: add except when file load error occurred

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import sys
 import argparse
 from src.utils.convert_md_to_html import convert_md_to_html
 from src.utils.extract_code_from_html import extract_code_from_html
+from src.utils.validate_is_markdown import validate_is_markdown
 from src.executor import executor
 
 
 def main(file_path):
-  with open(file_path) as f:
-    md = f.read()
+  try:
+    with open(file_path) as f:
+      md = f.read()
+  except:
+    print("[ERROR]: An error occurred while loading the file")
+
   html = convert_md_to_html(md)
   code_list = extract_code_from_html(html)
   executor(code_list)
@@ -22,6 +27,10 @@ if __name__ == "__main__":
 
   if not file_path:
     print("[ERROR]: --path argument not found")
+    sys.exit()
+  
+  if not validate_is_markdown(file_path):
+    print("[ERROR]: This file is not a markdown")
     sys.exit()
 
   main(file_path)
